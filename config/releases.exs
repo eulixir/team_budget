8a561cfdc679d84b0ca823feb31bf9d471223b88
João Pedro Alves <jotalmeida007@hotmail.com>
2021-06-02 16:49:40 -0300

    First deploy

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,7 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -12,7 +12,6 @@ database_url =
     """
 
 config :team_budget, TeamBudget.Repo,
-  # ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
@@ -27,15 +26,8 @@ config :team_budget, TeamBudgetWeb.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
-  ],
+    ],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
   secret_key_base: secret_key_base
 
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :team_budget, TeamBudgetWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
+config :team_budget, TeamBudgetWeb.Endpoint, server: true
