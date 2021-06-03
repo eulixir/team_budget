6daebe678245c40ba8a2d8da934597ac5f64a95f
João Pedro Alves <jotalmeida007@hotmail.com>
2021-06-03 12:13:56 -0300

    Add deps

--- mix.exs
+++ mix.exs
@@ -43,8 +43,12 @@ defmodule TeamBudget.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
-
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:absinthe, "~> 1.6"},
+      {:absinthe_relay, "~> 1.5", override: true},
+      {:absinthe_plug, "~> 1.5", override: true},
+      {:absinthe_phoenix, "~> 2.0"},
+      {:absinthe_error_payload, "~> 1.1"}
     ]
   end
 
